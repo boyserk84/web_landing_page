b6cc7d32cb1835a23d144d3f97ebe6a01bd2b274
Nate Kemavaha <me@nateserk.com>
2015-09-07 15:30:26 -0700

    Add configuration data file.

---
+++ client/js/config.js
@@ -0,0 +1,24 @@
+var DATA =
+	{
+		title: "Coming Soon. Landing Page!",
+		header: "Aenean vel purus magna",
+		body: " Lorem Disrem Curabitur in sagittis eros. Pellentesque in rhoncus ex. Nullam nec erat nec lectus fermentum ullamcorper. Donec aliquam tellus eros, sed fermentum lectus suscipit eget. Nullam odio mauris, viverra et urna eget, congue scelerisque turpis.",
+		footer:"Copyright 2015 LandingPage. All rights reserved.",
+		popUp: {
+				success: {
+					header: "Congratulation !",
+					body: "Lorem Disrem Curabitur in saggitss eros.",
+					button: "Ok"
+				},
+				fail: {
+					header: "Failed ",
+					body: "Missing or invalid value!",
+					button: "Retry"
+				},
+				error: {
+					header: "Error",
+					body: "Please try again later!",
+					button: "Retry"
+				}
+		}
+	};

f4b72f9bdd91f1fdd2948710a2bdd3bd6090c632
Nate Kemavaha <me@nateserk.com>
2015-09-09 01:07:34 -0700

    Using config.js to store configuration from Parse and other 3rd party constants and values.

--- client/js/config.js
+++ client/js/config.js
@@ -1,24 +1,11 @@
-var DATA =
-	{
-		title: "Coming Soon. Landing Page!",
-		header: "Aenean vel purus magna",
-		body: " Lorem Disrem Curabitur in sagittis eros. Pellentesque in rhoncus ex. Nullam nec erat nec lectus fermentum ullamcorper. Donec aliquam tellus eros, sed fermentum lectus suscipit eget. Nullam odio mauris, viverra et urna eget, congue scelerisque turpis.",
-		footer:"Copyright 2015 LandingPage. All rights reserved.",
-		popUp: {
-				success: {
-					header: "Congratulation !",
-					body: "Lorem Disrem Curabitur in saggitss eros.",
-					button: "Ok"
-				},
-				fail: {
-					header: "Failed ",
-					body: "Missing or invalid value!",
-					button: "Retry"
-				},
-				error: {
-					header: "Error",
-					body: "Please try again later!",
-					button: "Retry"
-				}
-		}
-	};
+/**
+ *  Configuration Data
+ * 
+ *  Any configuration data we'd like to define for any dependency or 3rd party constants and values.
+ * 
+ */
+var CONFIG = 
+{
+	appId: "<YOUR_PARSE_APP_ID_HASH>",
+	appKey: "<YOUR_PARSE_APP_KEY>"
+};

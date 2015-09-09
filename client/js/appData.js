22faf8cee4a651571218a02b2b1d80d0dc857fd2
Nate Kemavaha <me@nateserk.com>
2015-09-09 01:05:19 -0700

    Move data to its own file.

---
+++ client/js/appData.js
@@ -0,0 +1,30 @@
+/**
+ * Application data
+ * 
+ * Anything we'd like to define for your landing page. 
+ * 
+ */
+var DATA =
+{
+		title: "Coming Soon. Landing Page!",
+		header: "Aenean vel purus magna",
+		body: " Lorem Disrem Curabitur in sagittis eros. Pellentesque in rhoncus ex. Nullam nec erat nec lectus fermentum ullamcorper. Donec aliquam tellus eros, sed fermentum lectus suscipit eget. Nullam odio mauris, viverra et urna eget, congue scelerisque turpis.",
+		footer:"Copyright 2015 LandingPage. All rights reserved. Written by Nate K.",
+		popUp: {
+				success: {
+					header: "Congratulation !",
+					body: "Lorem Disrem Curabitur in saggitss eros.",
+					button: "Ok"
+				},
+				fail: {
+					header: "Invalid input !",
+					body: "Nullam nec erat nec lectus fermentum ullamcorper.",
+					button: "Retry"
+				},
+				error: {
+					header: "Oops ! Unexpected Error!",
+					body: "Sorry ! there is the issue on our part. Please try again later!",
+					button: "Try Again!"
+				}
+		}
+};
